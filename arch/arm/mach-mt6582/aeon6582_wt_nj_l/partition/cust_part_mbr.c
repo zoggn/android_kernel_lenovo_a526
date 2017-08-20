cd17e2bb5f89938b5850c71626028c8199952990
zoggn <kurkov.nikita@gmail.com>
2017-08-20 15:51:35 +0300

    a526: change MBR start addr

--- arch/arm/mach-mt6582/aeon6582_wt_nj_l/partition/cust_part_mbr.c
+++ arch/arm/mach-mt6582/aeon6582_wt_nj_l/partition/cust_part_mbr.c
@@ -1,5 +1,5 @@
 #include <partition_define.h>
 #include <linux/types.h>
 
-u64 MBR_START_ADDRESS_BYTE = 20480 * 1024;
+u64 MBR_START_ADDRESS_BYTE = 12288 * 1024;
 //u64 MBR_START_ADDRESS_BYTE = 8704 * 1024;
